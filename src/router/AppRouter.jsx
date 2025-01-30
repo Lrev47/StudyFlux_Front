3602d6cf8760d3d9c6b38e4225b054b8ca018f32
Lrev47 <lrev4766@gmail.com>
2025-01-30 15:38:53 -0600

    adding routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,29 +1,33 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Homepage from '../Pages/HomePage/HomePage.jsx'
-import Header from '../Components/Header/Header.jsx';
-import Footer from '../Components/Footer/Footer.jsx';
-import Login from '../Pages/LogIn/LogIn.jsx';
-import SignUp from '../Pages/SignUp/SignUp.jsx';
-import TierPage from '../Pages/TierPage/TierPage.jsx';
-
-
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "../Pages/HomePage/HomePage.jsx";
+import Header from "../Components/Header/Header.jsx";
+import Footer from "../Components/Footer/Footer.jsx";
+import Login from "../Pages/LogIn/LogIn.jsx";
+import SignUp from "../Pages/SignUp/SignUp.jsx";
+import TierPage from "../Pages/TierPage/TierPage.jsx";
+import ContactUs from "../Pages/ContactUs/ContactUs.jsx";
+import NotFoundPage from "../Pages/404Page/404Page.jsx";
+import TermsOfService from "../Pages/TermsOfService/TermsOfService.jsx";
+import Dashboard from "../Pages/Dashboard/Dashboard.jsx";
 
 function AppRouter() {
-
-    return(
-        <BrowserRouter>
-        <Header/>
-        <Routes>
-            <Route path="/" element={<Homepage/>}/>
-            <Route path="/login" element={<Login />} />
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/tiers" element={<TierPage />} />
-        </Routes>
-        <Footer/>
-        </BrowserRouter>
-    );
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/terms" element={<TermsOfService />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default AppRouter;
