6f7d6647b8e171c568646106f28d7bd21a198081
Lrev47 <lrev4766@gmail.com>
2025-01-30 15:42:48 -0600

    Modified HomePage.jsx

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import Hero from './Components/Hero.jsx';
-import FeaturesSection from './Components/Features.jsx';
-import TestimonialsSection from './Components/Testimonials.jsx';
-import StudyTipsSection from './Components/StudyTips.jsx';
-import CallToAction from './Components/CallToAction.jsx';
-import "./Style/HomePage.css"
+import React from "react";
+import Hero from "./Components/Hero.jsx";
+import FeaturesSection from "./Components/Features.jsx";
+import TestimonialsSection from "./Components/Testimonials.jsx";
+import StudyTipsSection from "./Components/StudyTips.jsx";
+import CallToAction from "./Components/CallToAction.jsx";
+import "./Style/HomePage.css";
 
 function Homepage() {
   return (
@@ -13,8 +13,7 @@ function Homepage() {
       <FeaturesSection />
       <TestimonialsSection />
       <StudyTipsSection />
-      <CallToAction/>
-
+      <CallToAction />
     </>
   );
 }
