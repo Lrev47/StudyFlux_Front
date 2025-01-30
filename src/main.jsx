20db524a0bb1b9edc0f8e2161c3796e1891913d6
Lrev47 <lrev4766@gmail.com>
2025-01-30 15:38:53 -0600

    added store

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-// src/main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './Store/Store.js'; // Named export from store.js
-import App from './App.jsx';              // We'll define a default export in App.jsx
-import "./Style/global.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./Store/Store.js"; // Named export from store.js
+import App from "./App.jsx"; // We'll define a default export in App.jsx
+import "./Style/global.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
